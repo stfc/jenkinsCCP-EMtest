fa8165003d5e54d06fe5c02bb89665e233c90d4f
Tom Dack <t-j-d@hotmail.co.uk>
2017-02-13 16:42:43 +0000

    Added first test files
    Initial commit of two versions of the Jenkinsfile.

---
+++ CCP-EM.groovy
@@ -0,0 +1,13 @@
+// CCP-EM pipeline
+
+#!groovy
+
+node {
+    stage('Load Modules'){
+        sh 'module load gcc cmake lapack blas'
+    }
+    stage('Checkout') {
+        sh 'bzr co bzr+http://oisin.rc-harwell.ac.uk/bzr/devtools/trunk devtools\n\
+                cd devtools/'
+    }
+}
